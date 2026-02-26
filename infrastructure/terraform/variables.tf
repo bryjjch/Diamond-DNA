54f6e463659d89b3d84fc75020cc63b904bfe0e4
Bryan <144505412+bryjjch@users.noreply.github.com>
2026-02-25 20:44:27 -0800

    changes to ingestion script and corresponding infrastructure

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -13,7 +13,7 @@ variable "environment" {
 variable "name_prefix" {
   description = "Prefix for resource names"
   type        = string
-  default     = "diamonddna"
+  default     = "diamond-dna"
 }
 
 # S3 Configuration
