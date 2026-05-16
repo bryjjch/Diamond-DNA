abf4710747803da628668fd393bcd4ed621e926d
Bryan <144505412+bryjjch@users.noreply.github.com>
2026-05-15 22:48:31 -0400

    Formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ provider "aws" {
 module "s3" {
   source = "./modules/s3"
 
-  data_lake_bucket_name       = var.data_lake_bucket_name
+  data_lake_bucket_name = var.data_lake_bucket_name
 
   tags = var.tags
 }
@@ -26,21 +26,21 @@ module "s3" {
 module "lambda" {
   source = "./modules/lambda"
 
-  name_prefix                    = var.name_prefix
-  data_lake_bucket_name          = module.s3.data_lake_bucket_name
-  data_lake_bucket_arn           = module.s3.data_lake_bucket_arn
-  s3_prefix                      = var.statcast_ingestion_s3_prefix
-  silver_s3_prefix               = var.statcast_silver_s3_prefix
-  gold_s3_prefix                 = var.statcast_gold_s3_prefix
-  schedule_expression            = var.statcast_ingestion_schedule_expression
-  by_player_schedule_expression  = var.statcast_by_player_schedule_expression
-  memory_size                    = var.statcast_ingestion_memory_size
-  timeout                        = var.statcast_ingestion_timeout
-  by_player_memory_size          = var.statcast_by_player_memory_size
-  by_player_timeout              = var.statcast_by_player_timeout
-  log_retention_days             = var.log_retention_days
-  image_tag                      = var.statcast_ingestion_image_tag
-  by_player_image_tag            = var.statcast_by_player_image_tag
+  name_prefix                   = var.name_prefix
+  data_lake_bucket_name         = module.s3.data_lake_bucket_name
+  data_lake_bucket_arn          = module.s3.data_lake_bucket_arn
+  s3_prefix                     = var.statcast_ingestion_s3_prefix
+  silver_s3_prefix              = var.statcast_silver_s3_prefix
+  gold_s3_prefix                = var.statcast_gold_s3_prefix
+  schedule_expression           = var.statcast_ingestion_schedule_expression
+  by_player_schedule_expression = var.statcast_by_player_schedule_expression
+  memory_size                   = var.statcast_ingestion_memory_size
+  timeout                       = var.statcast_ingestion_timeout
+  by_player_memory_size         = var.statcast_by_player_memory_size
+  by_player_timeout             = var.statcast_by_player_timeout
+  log_retention_days            = var.log_retention_days
+  image_tag                     = var.statcast_ingestion_image_tag
+  by_player_image_tag           = var.statcast_by_player_image_tag
 
   tags = var.tags
 
